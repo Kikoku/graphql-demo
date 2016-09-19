e06eac7c9a72597cc67171cfe4d9f4f29682835c
Collin <candrew022@gmail.com>
2016-09-19 09:32:14 -0400

    Create Server

--- server.js
+++ server.js
@@ -1,54 +1,13 @@
-// Import the required libraries
-import {
-  graphql,
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString
-} from 'graphql';
-var graphqlHTTP = require('express-graphql');
+import { graphql } from 'graphql';
 import express from 'express';
-// Import the data you created above
-var data = require('./data.json');
+import graphQLHTTP from 'express-graphql';
+import Schema from './schema';
 
-// Define the User type with two string fields: `id` and `name`.
-// The type of User is GraphQLObjectType, which has child fields
-// with their own types (in this case, GraphQLString).
-var userType = new GraphQLObjectType({
-  name: 'User',
-  fields: {
-    id: { type: GraphQLString },
-    name: { type: GraphQLString },
-  }
-});
+let app = express();
+const PORT = process.env.PORT || 8080;
 
-// Define the schema with one top-level field, `user`, that
-// takes an `id` argument and returns the User with that ID.
-// Note that the `query` is a GraphQLObjectType, just like User.
-// The `user` field, however, is a userType, which we defined above.
-var schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-      user: {
-        type: userType,
-        // `args` describes the arguments that the `user` query accepts
-        args: {
-          id: { type: GraphQLString }
-        },
-        // The resolve function describes how to "resolve" or fulfill
-        // the incoming query.
-        // In this case we use the `id` argument from above as a key
-        // to get the User from `data`
-        resolve: function (_, args) {
-          return data[args.id];
-        }
-      }
-    }
-  })
-});
+app.use('/', graphQLHTTP({schema: Schema, pretty: true, graphiql: true}));
 
-express()
-  .use('/graphql', graphqlHTTP({ schema: schema, pretty: true, graphiql: true }))
-  .listen(3000);
-
-console.log('GraphQL server running on http://localhost:3000/graphql');
+app.listen(8080, (err) => {
+  console.log(`GraphQL Server is now running on ${PORT}`);
+})
