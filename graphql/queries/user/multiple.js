17d48513a51f197d7812749bd933d02661febe7d
Collin <candrew022@gmail.com>
2016-09-22 12:19:41 -0400

    Update multiple to use model find funciton

--- graphql/queries/user/multiple.js
+++ graphql/queries/user/multiple.js
@@ -7,27 +7,5 @@ import User from '../../models/user'
 
 export default {
   type: new GraphQLList(UserType),
-  resolve: () => {
-    return new Promise((resolve, reject) => {
-      User.find().populate({
-        path: 'friends',
-        model: 'User',
-        populate: {
-          path: 'friends',
-          model: 'User',
-          populate: {
-            path: 'friends',
-            model: 'User',
-            populate: {
-              path: 'friends',
-              model: 'User'
-            }
-          }
-        }
-      }).exec((err, users) => {
-        if(err) reject(err)
-        else resolve(users)
-      });
-    });
-  }
+  resolve: User.getUsersList
 }
