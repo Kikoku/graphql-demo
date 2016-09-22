525d3339ffaceb69f4aa246fb1e235f8c39acea8
Collin <candrew022@gmail.com>
2016-09-22 12:20:14 -0400

    Add resolvers to bestFriend and friends fields

--- graphql/types/user.js
+++ graphql/types/user.js
@@ -4,14 +4,21 @@ import {
   GraphQLString,
   GraphQLList
 } from 'graphql';
+import User from '../models/user';
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    bestFriend: { type: UserType },
-    friends: { type: new GraphQLList(UserType)}
+    bestFriend: {
+       type: UserType,
+       resolve: user => User.getUserById(user.bestFriend)
+    },
+    friends: {
+      type: new GraphQLList(UserType),
+      resolve: user => user.friends.map(User.getUserById)
+    }
   })
 });
 
