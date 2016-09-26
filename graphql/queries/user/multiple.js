a93c3fa04ad8427989ed39d5ee841407a8db8a41
Collin <candrew022@gmail.com>
2016-09-25 23:53:56 -0400

    Resolve to mongoose User model

--- graphql/queries/user/multiple.js
+++ graphql/queries/user/multiple.js
@@ -3,9 +3,9 @@ import {
 } from 'graphql';
 
 import UserType from '../../types/user';
-import User from '../../models/user'
+import User from '../../../models/user';
 
 export default {
   type: new GraphQLList(UserType),
-  resolve: User.getUsersList
-}
+  resolve: () => User.findAsync()
+};
