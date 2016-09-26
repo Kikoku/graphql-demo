07cb69c57cd211988964843dcae2afc5d9c452b8
Collin <candrew022@gmail.com>
2016-09-26 12:33:10 -0400

    Convert user object to JSON

--- loaders.js
+++ loaders.js
@@ -1,3 +1,9 @@
 import User from './models/user';
 
-export const getUserById = (id) => User.findByIdAsync(id);
+export const getUserById = (id) => User.findByIdAsync(id).then(user => {
+  user = user.toJSON();
+  user.id = user._id.toString();
+  user.friends = user.friends.map(friend => friend.toString());
+  user.bestFriend = user.bestFriend ? user.bestFriend.toString() : null;
+  return user;
+});
