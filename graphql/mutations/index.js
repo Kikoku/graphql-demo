8379d5e51a5789da99b2f8a80d4e9da1ba293c65
Collin <candrew022@gmail.com>
2016-09-25 23:51:57 -0400

    Remove mutations

--- graphql/mutations/index.js
+++
@@ -1,9 +0,0 @@
-import todo from './todo';
-import comment from './comment';
-import user from './user';
-
-export default {
-  ...todo,
-  ...comment,
-  ...user
-}
--- graphql/mutations/user/index.js
+++
@@ -1,9 +0,0 @@
-import addUser from './add';
-import updateBestFriend from './updateBestFriend';
-import pushFriend from './pushFriend'
-
-export default {
-  addUser,
-  updateBestFriend,
-  pushFriend
-}
