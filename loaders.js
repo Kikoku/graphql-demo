283d5a4ce2d757d0266301f4356bc49ce858cbb2
Collin <candrew022@gmail.com>
2016-09-28 15:40:08 -0400

    Create multiple user loader

--- loaders.js
+++ loaders.js
@@ -7,3 +7,15 @@ export const getUserById = (id) => User.findByIdAsync(id).then(user => {
   user.bestFriend = user.bestFriend ? user.bestFriend.toString() : null;
   return user;
 });
+
+export const getUsers = () => User.findAsync()
+.then(users => {
+  users.forEach(user => {
+    user = user.toJSON();
+    user.id = user._id.toString();
+    user.friends = user.friends.map(friend => friend.toString());
+    user.bestFriend = user.bestFriend ? user.bestFriend.toString() : null;
+    return user
+  })
+  return users;
+})
