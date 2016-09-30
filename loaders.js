458946166e665ea44b377e17af11b9b3123cf9b4
Collin <candrew022@gmail.com>
2016-09-30 15:14:12 -0400

    Revert "DRY user sanatize function"
    This reverts commit 1c5ca2a7158c898760225136262c128a6d1190d1.

--- loaders.js
+++ loaders.js
@@ -1,14 +1,21 @@
 import User from './models/user';
 
+export const getUserById = (id) => User.findByIdAsync(id).then(user => {
   user = user.toJSON();
   user.id = user._id.toString();
   user.friends = user.friends.map(friend => friend.toString());
   user.bestFriend = user.bestFriend ? user.bestFriend.toString() : null;
   return user;
-}
-
+});
 
 export const getUsers = () => User.findAsync()
 .then(users => {
+  users.forEach(user => {
+    user = user.toJSON();
+    user.id = user._id.toString();
+    user.friends = user.friends.map(friend => friend.toString());
+    user.bestFriend = user.bestFriend ? user.bestFriend.toString() : null;
+    return user
+  })
   return users;
 })
