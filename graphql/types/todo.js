1f38868635a9184d86dd5d8ba8f8e032e03cec0c
Collin <candrew022@gmail.com>
2016-10-03 11:27:05 -0400

    Create TodoType

---
+++ graphql/types/todo.js
@@ -0,0 +1,26 @@
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList
+} from 'graphql';
+
+import UserType from './user';
+
+const TodoType = new GraphQLObjectType({
+  name: 'Todo',
+  fields: () => ({
+    id: {
+      type: GraphQLID
+    },
+    title: {
+      type: GraphQLString
+    },
+    author: {
+      type: UserType,
+      resolve: (todo, args, {loaders}) => loaders.user.load(todo.author)
+    }
+  })
+})
+
+export default TodoType;
