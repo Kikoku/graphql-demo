63507b85525d4a3bd974202105041c2eab758186
Collin <candrew022@gmail.com>
2016-10-09 15:57:47 -0400

    Enable CORS

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ mongoose.connect('mongodb://localhost/graphql', (err) => {
 let app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  console.log('headers set');
+  next();
+})
+
 app.use('/', graphQLHTTP( req => {
 
   const cacheMap = new Map();
