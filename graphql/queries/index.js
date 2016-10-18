8b389fbaf1ef8bae6e60db9789a1d2abd88c7ea7
Collin <candrew022@gmail.com>
2016-10-18 11:36:44 -0400

    Add search to index query

--- graphql/queries/index.js
+++ graphql/queries/index.js
@@ -1,7 +1,9 @@
 import user from './user';
-import todo from './todo'
+import todo from './todo';
+import search from './search';
 
 export default {
   ...user,
-  ...todo
+  ...todo,
+  ...search
 }
