b6e42b3fabb562384c7f73f7e92b0e2abb28da49
Collin <candrew022@gmail.com>
2016-12-07 00:35:36 -0500

    Update Query in schema to relay spec

--- graphql/index.js
+++ graphql/index.js
@@ -1,14 +1,95 @@
 import {
   GraphQLObjectType,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLList,
+  GraphQLInt
 } from 'graphql';
-import queries from './queries';
+import {
+  connectionDefinitions,
+  connectionFromPromisedArray,
+  connectionFromArray,
+  connectionArgs,
+  fromGlobalId,
+  nodeDefinitions
+} from 'graphql-relay';
+
 import mutations from './mutations';
+import User from '../models/user'
+
+function getObjectsByType(type, args) {
+  var objects = [];
+  var totalCount = 0;
+
+  return {
+    objects: User.findAsync(),
+    totalCount: User.countAsync()
+  }
+}
+
+function getObjectByTypeAndId(type, id) {
+  return User.findByIdAsync(id)
+}
+
+function typeToGraphQLType(type) {
+  var UserType = require('./types/user');
+
+  switch (type) {
+    case 'user':
+      return UserType.default
+  }
+}
+
+function rootFieldById(idName, type) {
+  var getter = (id) => User.findByIdAsync(id);
+  var argDefs = {};
+  argDefs.id = {type: GraphQLID};
+  argDefs[idName] = {type: GraphQLID};
+
+  return {
+    type: typeToGraphQLType(type),
+    args: argDefs,
+    resolve: (_, args) => {
+
+      if(args[idName] !== undefined && args[idName] !== null) {
+        return getter(args[idName])
+      }
+
+      if(args.id !== undefined && args[idName] !== null) {
+        return getter(args.id)
+      }
+
+      throw new Error(`must provide id or ${idName}`)
+    }
+  }
+}
+
+function rootConnection(name, type) {
+  var graphqlType = typeToGraphQLType(type);
+  var {connectionType} = connectionDefinitions({
+    name: name,
+    nodeType: graphqlType
+  });
+
+  return {
+    type: connectionType,
+    args: connectionArgs,
+    resolve: (_, args) => {
+      var { objects, totalCount } = getObjectsByType(type, args)
+      return connectionFromPromisedArray(User.findAsync(),args)
+    }
+  }
+}
+
+const rootFields = () => ({
+  allusers: rootConnection('Users', 'user'),
+  user: rootFieldById('user', 'user')
+})
 
 export default new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
-    fields: queries
+    fields: rootFields
   }),
   mutation: new GraphQLObjectType({
     name: 'Mutation',
