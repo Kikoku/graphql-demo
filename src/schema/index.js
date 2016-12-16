d32405bbb9536d58ffb1bc2156cce932213c11a3
Collin <candrew022@gmail.com>
2016-12-16 15:06:14 -0500

    Add Todo Queries

--- src/schema/index.js
+++ src/schema/index.js
@@ -10,9 +10,11 @@ import {
   connectionFromPromisedArray,
   connectionArgs
 } from 'graphql-relay';
-import userLoader from './apiHelpers.js'
+import userLoader, { todoLoader } from './apiHelpers';
 import UserType, { UserConnection } from './types/user';
+import TodoType, { TodoConnection } from './types/todo';
 import User from '../../models/user';
+import Todo from '../../models/todo';
 import { nodeField } from './node';
 
 const queryType = new GraphQLObjectType({
@@ -36,7 +38,25 @@ const queryType = new GraphQLObjectType({
         User.findAsync(),
         args
       )
-    }
+    },
+    todo: {
+      type: TodoType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+          description: 'ID of a Todo'
+        }
+      },
+      resolve: (_, args) => todoLoader.load(args.id)
+    },
+    todos: {
+      type: TodoConnection,
+      args: connectionArgs,
+      resolve: (_, args) => connectionFromPromisedArray(
+        Todo.findAsync(),
+        args
+      )
+    },
   })
 })
 
