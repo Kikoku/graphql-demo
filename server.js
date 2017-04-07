19424467ac097a5aef07405fa1db55f45ceac4e1
Collin <candrew022@gmail.com>
2017-04-07 08:54:14 -0400

    Run createLoader constructor

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/', cors(), graphQLHTTP( req => {
   return {
     context: {
       viewer,
-      ...createLoaders
+      ...createLoaders()
     },
     schema: schema,
     pretty: true,
