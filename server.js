421a77cb8c7cf311924c04de3c9ae76769ab4183
Collin <candrew022@gmail.com>
2017-04-13 12:03:12 -0400

    Setup jwt session middleware
    // TODO: setup actual user athentication
    // TODO: Promisify token verification check
    // TODO: store _r token information in DB to allow for invalidation

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ import cors from 'cors';
 import Dataloader from 'dataloader';
 import createLoaders from './loaders';
 import jwt from 'jsonwebtoken';
+import User from './models/user'
 
 let app = express();
-app.disable('x-powered-by')
+app.disable('x-powered-by');
+if (app.get('env') === 'development') {
+  require('dotenv').config();
+}
 const PORT = process.env.PORT || 8080;
 
 mongoose.connect('mongodb://localhost/graphql', (err) => {
@@ -26,22 +30,59 @@ mongoose.connect('mongodb://localhost/graphql', (err) => {
 
 })
 
-app.use('/', (req, res, next) => {
+app.post('/login', (req, res) => {
+  const _r = jwt.sign(
+    {
+      userId: "57e2900d04d8790dc84243f0"
+    },
+    process.env.REFRESH_SECRET,
+    { expiresIn: '30d' }
+  );
 
+  res.set({
+    '_r': _r
+  })
+  res.sendStatus(200)
+})
+
+app.use((req, res, next) => {
+
+  let _t = req.headers._t;
+  let _r = req.headers._r;
   req.viewer = {
-    name: 'guest',
-    id: 'guest'
+    name: 'guest'
   }
 
-  if(req.headers.authorization) {
+  if(!_t) {
+    if(_r) {
+      try {
+        _r = jwt.verify(req.headers._r, process.env.REFRESH_SECRET);
+        User.findById(_r.userId)
+        .exec((err, user) => {
+          if(err) console.log(err)
+          req.viewer = user
+          res.set({
+            _t: jwt.sign(
+              user,
+              process.env.ACCESS_TOKEN_SECRET,
+              { expiresIn: '5m'}
+            )
+          })
+        })
+      } catch(err) {
+        console.log(err);
+      }
+    }
+  } else {
     try {
-      req.viewer = jwt.verify(req.headers.authorization, 'secret')
+      req.viewer = jwt.verify(req.headers._t, process.env.ACCESS_TOKEN_SECRET)._doc
     } catch(err) {
-      // NOTE: Catch errors
+      console.log(err);
     }
   }
 
   next();
+
 })
 
 app.use('/', cors(), graphQLHTTP( req => {
